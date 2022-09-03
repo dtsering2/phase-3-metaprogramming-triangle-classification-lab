b73346d4711fcb7df87dbc71af50697396c2faa4
Tsering Tsering <dtsering2@gmail.com>
2022-09-02 20:35:26 -0400

    commit tree 28a078c99c3c9c141191768ad01f444b1164f3b3

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
-  # write code here
+  attr_reader :sides
+
+  def initialize(s1,s2,s3)
+    @sides = [s1,s2,s3]
+  end
+
+  def kind
+    validate_tri
+    
+    if @sides.uniq.length == 1
+      :equilateral
+    elsif @sides.uniq.length == 2
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def negative_zero_check 
+    @sides.all?(&:positive?)
+  end
+
+  def triangle_inequality
+    @sides[0]+@sides[1]>@sides[2] &&@sides[0]+@sides[2]>@sides[1]&&@sides[1]+@sides[2]>@sides[0]
+  end
+  
+  def validate_tri
+    raise TriangleError unless negative_zero_check && triangle_inequality
+  end
+
+  class TriangleError < StandardError
+  end
 end
